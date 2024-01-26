2f0b6e27aba72716ea60684e10eaa367b7745d65
Md. Mozammal Haq <mrmozammal@gmail.com>
2024-01-26 08:59:18 +0600

    Add Dark & Light mode theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Movielist from "./cine/Movielist";
-import { MovieContext } from "./context";
+import Page from "./Page";
+import { MovieContext, ThemeContext } from "./context";
 
 export default function App() {
   const [cartData, setCartData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <>
-      <MovieContext.Provider value={{ cartData, setCartData }}>
-        <Header />
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <Movielist />
-        </div>
-      </MovieContext.Provider>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Page />
+        </MovieContext.Provider>
+      </ThemeContext.Provider>
     </>
   );
 }
